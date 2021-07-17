ce9b25fb0d66836be00bfbf3f442179e8e22722c
car-r <carson.ri@protonmail.com>
2021-07-16 17:22:59 -0700

    starting header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/ui/header';
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Breaking Bad</h1>
     </div>
   );
